8698bf3df1461cf700dea56cfd675e0213741c42
Chris Miklius <cmiklius@gmail.com>
2019-01-08 11:46:06 -0600

    complete refactored version of logic

--- logic.rb
+++ logic.rb
@@ -29,16 +29,22 @@ end
 COLOR_OPTIONS = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 def new_game()
-
-
   computer_pattern = [COLOR_OPTIONS[rand(0..5)], COLOR_OPTIONS[rand(0..5)], COLOR_OPTIONS[rand(0..5)], COLOR_OPTIONS[rand(0..5)]]
-
+  puts computer_pattern[0]
+  puts computer_pattern[1]
+  puts computer_pattern[2]
+  puts computer_pattern[3]
   game_start(computer_pattern)
 end
 
 def game_start(computer_pattern)
   puts "Welcome to Mastermind! The pattern has been decided. Make your first guess. Valid choices are red, orange, yellow, green, blue, and purple. Any other answers will lead to a wasted turn."
   round_count = 1
+  computer_pattern2 = []
+  computer_pattern2 << computer_pattern[0]
+  computer_pattern2 << computer_pattern[1]
+  computer_pattern2 << computer_pattern[2]
+  computer_pattern2 << computer_pattern[3]
 
   until round_count == 11
     puts "Make a guess!"
@@ -47,22 +53,56 @@ def game_start(computer_pattern)
     guess = guess.split(" ")
 
     count = 0
-    response = []
-    until count == 3
+    response = {}
+    until count == 4
+
       if computer_pattern[count] == guess[count]
-        response << "white peg"
-      elsif guess.include?(computer_pattern[count])
-        response << "red peg"
+        response[count] = 'white peg'
+        guess[count] = "white peg"
+        computer_pattern2[count] = "white peg"
+      else
+        response[count] = "nada"
       end
 
       count += 1
     end
 
+    count = 0
+
+    until count == 4
+      if guess[count] != "white peg" && computer_pattern2.include?(guess[count]) && response[count] != "white peg"
+
+        response[count] = "red peg"
+        computer_pattern2[computer_pattern2.index(guess[count])] = "red peg"
+      end
+
+      count += 1
+    end
+    puts "#{response}"
     puts "Guess received. Response is #{response[0]}, #{response[1]}, #{response[2]}, #{response[3]}"
+
+=begin
+      puts "The pattern is..."
+      computer_pattern.each do |p|
+          print p
+      end
+
+      puts "\n"
+
+      puts "The second pattern is..."
+      computer_pattern2.each do |l|
+          print l
+      end
+=end
 
     round_count += 1
 
-    return "CORRECTAMUNDO!" if response == ['white peg', 'white peg', 'white peg', 'white peg']
+    computer_pattern2 = []
+
+    if response == ['white peg', 'white peg', 'white peg', 'white peg']
+      puts "CORRECT"
+      return
+    end
   end
 end
 
